b4b43a1227156b81f10c21e774604dbec0d67098
Laís Matie <lais@jeraapps.com>
2021-05-06 16:23:38 -0400

    Front react todolist

---
+++ todolist/front/src/todo/todo.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react"
+import PageHeader from "../template/pageHeader"
+import TodoForm from "../todo/todoForm"
+import TodoList from "../todo/todoList"
+
+export default class Todo extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {description: "", list:[]}
+
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange= this.handleChange.bind(this)
+
+    }
+    
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+                <TodoForm
+                    description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                />
+                <TodoList />
+            </div>
+
+        )
+    }
+}
